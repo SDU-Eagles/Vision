1c33c99b53b21dfd5f6984cfdb0848a515e0a0a2
Simon C. Dockweiler <scdockweiler@gmail.com>
2020-04-06 16:42:52 +0200

    Changed to using file instead of webcam

--- Vision code/Vision.py
+++ Vision code/Vision.py
@@ -14,7 +14,7 @@ from os.path import dirname, abspath
 cap = cv.VideoCapture(dirname(dirname(dirname(abspath(__file__)))) + '/VisionSample/New (1).mp4')
 
 # Using webcam
-cap = cv.VideoCapture(0)
+#cap = cv.VideoCapture(0)
 
 # Using RTSP
 #cap = cv.VideoCapture("rtsp://192.168.42.1/live")

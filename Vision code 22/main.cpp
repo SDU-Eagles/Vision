1c0f90c046272e429b9b1711918e2ca3b35ad2eb
erkja19 <erkja19@student.sdu.dk>
2021-10-31 15:17:13 -0700

    arbejde i main cornerfind

--- Vision code 22/main.cpp
+++ Vision code 22/main.cpp
@@ -13,31 +13,51 @@ int main()
     cv::imshow("Display window",image);
     cv::waitKey(0);
 
+//  Isolated the red in the photo
     cv::Mat imageColorCvt;
     cv::cvtColor(image,imageColorCvt,cv::COLOR_BGR2HSV);
 
     cv::Mat imageGaus;
     cv::GaussianBlur(imageColorCvt,imageGaus,cv::Size(3,3),2,2);
 
-    //lower[155,25,0];
-    //upper[179,255,255];
-
-    //cv::inRange(lower,cv::Scalar(155,25,0),cv::Scalar(156,26,1),lower);
-    //cv::inRange(upper,cv::Scalar(179,255,255),cv::Scalar(180,255,255),upper);
-
-
     cv::Mat mask;
     cv::inRange(imageGaus,cv::Scalar(0,50,0),cv::Scalar(180,150,255),mask);
-
 
     cv::Mat result;
     cv::bitwise_and(image,image,result,mask=mask);
 
     cv::imshow("mask",mask);
     cv::waitKey(0);
-
     cv::imshow("result",result);
     cv::waitKey(0);
+
+//
+    int thresh = 200;
+    cv::Mat gray, dst, dstNorm, dstNormScaled;
+    cv::cvtColor(result,gray,cv::COLOR_BGR2GRAY);
+
+    dst = cv::Mat::zeros(result.size(),CV_32FC1);
+
+    cv::cornerHarris(gray,dst,7,5,0.05);
+
+    cv::normalize(dst,dstNorm,0,255,cv::NORM_MINMAX,CV_32FC1,cv::Mat());
+    cv::convertScaleAbs(dstNorm,dstNormScaled);
+
+    for( int j = 0; j < dstNorm.rows ; j++ )
+    {
+        for( int i = 0; i < dstNorm.cols; i++ )
+        {
+            if( (int) dstNorm.at<float>(j,i) > thresh )
+            {
+               cv::circle( dstNormScaled, cv::Point( i, j ), 5,  cv::Scalar(0), 2, 8, 0 );
+            }
+        }
+    }
+    std::cout << dstNormScaled << std::endl;
+    cv::namedWindow( "corners_window",cv::WINDOW_AUTOSIZE);
+    cv::imshow("corners_window",dstNormScaled);
+    cv::waitKey(0);
+
 
     return 0;
 }

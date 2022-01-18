aef9d5ada008c5e039346fd774433038b9297580
nthom18 <nthom18@student.sdu.dk>
2022-01-18 17:29:12 +0100

    Update

--- Vision code 22/nico_test.py
+++ Vision code 22/nico_test.py
@@ -3,7 +3,7 @@ from math import dist
 import numpy as np
 
 # path = "Markers/Marker1.png"
-path = "hsv-colours.png"
+path = "Sample_images/5.jpg"
 dim = (500, 500)
 img = cv2.imread(path)
 img = cv2.resize(img, dim, interpolation= cv2.INTER_LINEAR)
@@ -15,7 +15,7 @@ def inLabDist(img, colour, maxDist):
     colour = cv2.cvtColor( np.uint8([[colour]] ), cv2.COLOR_BGR2LAB)[0][0]
     mask = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-    # Loop throug image
+    # Loop through image
     for i, row in enumerate(mask):
         for j, pixel in enumerate(row):
             distance = dist(colour, pixel)
@@ -24,13 +24,32 @@ def inLabDist(img, colour, maxDist):
             else:
                 mask[i][j] = 255
     
-    return mask
+    return mask[:,:,1]
 
 
+def crop(bit_img):
+    top = []
+    left = []
+    right = []
+    bottom = []
 
-mask = inLabDist(img, [0, 0, 255], 100)
-# result = cv2.bitwise_and(img, img, mask = mask)
+    def get_top(): 
+        for i, row in enumerate(bit_img):
+            for j, pixel in enumerate(row):
+                if (pixel != 0):
+                    return [i,j]
 
-cv2.imshow('image', mask)
+    return get_top()
+
+
+mask = inLabDist(img, [0, 0, 255], 80)
+result = cv2.bitwise_and(img, img, mask = mask)
+
+print(crop(mask))
+# kernel = np.ones((4, 4), np.uint8)
+# result = cv2.erode(result, kernel) 
+# result = cv2.dilate(result, kernel)
+
+
+cv2.imshow('image', result)
 cv2.waitKey(0)
-

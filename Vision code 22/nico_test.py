ac77d7adda24fdef48fdf28c29a5646342acc779
PositiveBeat <nicoline@finman.dk>
2022-03-11 08:39:01 +0100

    Added colour circle image

--- Vision code 22/nico_test.py
+++ Vision code 22/nico_test.py
@@ -2,12 +2,6 @@ import cv2
 from math import dist
 import numpy as np
 
-# path = "Markers/Marker1.png"
-path = "hsv-colours.png"
-dim = (500, 500)
-img = cv2.imread(path)
-img = cv2.resize(img, dim, interpolation= cv2.INTER_LINEAR)
-
 
 def inLabDist(img, colour, maxDist):
 
@@ -20,17 +14,29 @@ def inLabDist(img, colour, maxDist):
         for j, pixel in enumerate(row):
             distance = dist(colour, pixel)
             if (distance > maxDist):
-                mask[i][j] = 0
-            else:
                 mask[i][j] = 255
+            else:
+                mask[i][j] = 0
     
     return mask
 
 
+if __name__ == '__main__':
+    
+    path = "Markers/Marker5.png"
+    # path = "Sample_images/2.png"
+    dim = (250, 250)
+    img = cv2.imread(path)
+    img = cv2.resize(img, dim, interpolation= cv2.INTER_LINEAR)
+    
+    
+    mask = inLabDist(img, [0, 0, 255], 100)
+    # result = cv2.bitwise_and(img, img, mask = mask)
 
-mask = inLabDist(img, [0, 0, 255], 100)
-# result = cv2.bitwise_and(img, img, mask = mask)
+    cv2.imshow('image', mask)
+    cv2.waitKey(0)
 
-cv2.imshow('image', mask)
-cv2.waitKey(0)
-
+    
+    
+    
+    
